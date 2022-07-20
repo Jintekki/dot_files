92b3a80de81cd606f054ff5554342e6816fdff2f
Jintekki <jintekki@gmail.com>
2022-07-20 00:02:36 -0400

    Edit to .doom.d config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,9 @@
+;;                  ██ ██ ███    ██ ████████ ███████ ██   ██ ██   ██ ██ 
+;;                  ██ ██ ████   ██    ██    ██      ██  ██  ██  ██  ██ 
+;; █████ █████      ██ ██ ██ ██  ██    ██    █████   █████   █████   ██ 
+;;             ██   ██ ██ ██  ██ ██    ██    ██      ██  ██  ██  ██  ██ 
+;;              █████  ██ ██   ████    ██    ███████ ██   ██ ██   ██ ██
+
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
