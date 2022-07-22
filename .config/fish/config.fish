3886f03ef6fd191c95b54020179ba2150a8892dd
Jintekki <jintekki@gmail.com>
2022-07-21 20:51:28 -0400

    Replace ls with exa in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,16 @@
 #             ██   ██ ██ ██  ██ ██    ██    ██      ██  ██  ██  ██  ██ 
 #              █████  ██ ██   ████    ██    ███████ ██   ██ ██   ██ ██
 
+### STARSHIP PROMPT ###
 if status is-interactive
    starship init fish | source
 end
+
+### ALIASES ###
+
+# exa - ls replacement #
+alias ls='exa -a --color=always --group-directories-first'  # all files and dirs
+alias la='exa -al --color=always --group-directories-first' # list format
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
